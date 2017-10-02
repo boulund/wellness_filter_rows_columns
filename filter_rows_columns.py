f881c4b06c691024608a2d2366d4094d056b6303
Fredrik Boulund <fredrik.boulund@ki.se>
2017-10-02 13:44:47 +0200

    Can now filter columns or rows, but not simultaneously.

--- filter_rows_columns.py
+++ filter_rows_columns.py
@@ -3,7 +3,9 @@
 # Fredrik Boulund 2017
 
 from sys import argv, exit
+from itertools import chain
 import argparse
+import numpy as np
 
 global filtered_rows
 
@@ -12,7 +14,7 @@ def parse_args():
     """
 
     desc = """Filter rows or columns from tab separated tables.
-           Assumes first row contains headers, and that the first column is the index colum
+           Assumes first row contains headers, and that the first column is the index column.
            Fredrik Boulund 2017. """
     parser = argparse.ArgumentParser(description=desc)
 
@@ -22,13 +24,15 @@ def parse_args():
             default="filtered_table.tsv",
             help="Output filename [%(default)s].")
     parser.add_argument("-r", "--rowsum", metavar="R",
-            default=5,
+            default=-1,
             type=float,
-            help="Minimum row sum to include row in output [%(default)s].")
+            help="Minimum row sum to include row in output, "
+                 "-1 skips row filtering [%(default)s].")
     parser.add_argument("-c", "--colsum", metavar="C",
-            default=5,
+            default=-1,
             type=float,
-            help="Minimum column sum to include row in output [%(default)s].")
+            help="Minimum column sum to include row in output, "
+                 "-1 skips column filtering [%(default)s].")
 
     if len(argv) < 2:
         parser.print_help()
@@ -41,8 +45,29 @@ def yield_rows(filename, separator="\t"):
     """Yield rows from filename.
     """
     with open(filename) as f:
+        yield f.readline().rstrip().split(separator)
         for line in f:
             yield line.strip().split(separator)
+
+
+class Column_filterer():
+    """Filter columns in a two-pass operation.
+    """
+    def __init__(self, num_columns):
+        self.filtered_columns = 0
+        self.num_columns = num_columns
+
+    def compute_colsums(self, rows, min_colsum):
+        self.col_sums = np.zeros(self.num_columns, dtype=np.int32)
+        for row in rows:
+            self.col_sums += np.array(row[1:], dtype=np.int32)
+        self.keep_cols = self.col_sums >= min_colsum
+
+    def filter_columns(self, rows):
+        for row in rows:
+            value_columns = np.array(row[1:], dtype=np.int32)
+            new_line = (row[0],), value_columns[self.keep_cols]
+            yield chain(*new_line) 
 
 
 class Row_filterer():
@@ -50,6 +75,7 @@ class Row_filterer():
     """
     def __init__(self):
         self.filtered_rows = 0
+        self.removed_rows = []
 
     def filter_rows(self, rows, min_rowsum):
         for row in rows:
@@ -57,26 +83,47 @@ class Row_filterer():
             if rowsum >= min_rowsum:
                 yield row
             else:
-                #print("Skipping row", row[0], "Rowsum", rowsum)
                 self.filtered_rows += 1
+                self.removed_rows.append(row[0])
 
 
 def main(table_fn, min_rowsum, min_colsum, outfile, separator="\t"):
-    """Filter rows from tab separated table.
+    """Filter rows and/or columns from tab separated table.
     """
 
-    rows = yield_rows(table_fn)
-    header_line = next(rows)
+    if min_rowsum > -1 and min_colsum == -1:
+        rows = yield_rows(table_fn)
+        header_line = next(rows)
+        row_filterer = Row_filterer()
+        filtered_rows = row_filterer.filter_rows(rows, min_rowsum)
 
-    row_filterer = Row_filterer()
-    filtered_rows = row_filterer.filter_rows(rows, min_rowsum)
+        print("Writing filtered table to", outfile)
+        with open(outfile, 'w') as out:
+            out.write(separator.join(header_line)+"\n")
+            for filtered_row in filtered_rows:
+                out.write(separator.join(str(v) for v in filtered_row)+"\n")
+        print("Filtered", row_filterer.filtered_rows, "rows.")
+        print("Filtered row(s):", "\t".join(row_filterer.removed_rows))
+    elif min_colsum > -1 and min_rowsum == -1:
+        # Two-pass column filtering
+        rows2 = yield_rows(table_fn)
+        num_columns = len(next(rows2))-1
+        column_filterer = Column_filterer(num_columns)
+        column_filterer.compute_colsums(rows2, min_colsum)
 
-    print("Writing filtered table to", outfile)
-    with open(outfile, 'w') as out:
-        out.write(separator.join(header_line)+"\n")
-        for filtered_row in filtered_rows:
-            out.write(separator.join(str(v) for v in filtered_row)+"\n")
-    print("Filtered", row_filterer.filtered_rows, "rows.")
+        rows3 = yield_rows(table_fn)
+        column_headers = np.array(next(rows3))
+        if not column_headers[0]:
+            column_headers[0] = " " # Workaround for empty first column title
+        with open(outfile, 'w') as out:
+            out.write("\t".join(chain(column_headers[0], 
+                    column_headers[1:][column_filterer.keep_cols]))+"\n")
+            for row in column_filterer.filter_columns(rows3):
+                out.write("\t".join(str(v) for v in row)+"\n")
+        print("Filtered", column_filterer.num_columns - sum(column_filterer.keep_cols), "columns.")
+        print("Filtered column(s):", "\t".join(column_headers[1:][np.invert(column_filterer.keep_cols)]))
+    elif (min_colsum > -1 and min_rowsum > -1) or (min_colsum == -1 and min_rowsum == -1):
+        print("Code currently cannot do both row and column filtering at the same time.")
 
 
 if __name__ == "__main__":
